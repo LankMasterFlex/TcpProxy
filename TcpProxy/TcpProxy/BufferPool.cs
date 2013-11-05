b428a8729dd695938ad723bc375b32da5c1b75e6
Rajan Grewal <yellondeil2@gmail.com>
2013-11-04 19:36:13 -0500

    Initial

---
+++ TcpProxy/TcpProxy/BufferPool.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace TcpProxy
+{
+    internal sealed class BufferPool
+    {
+        private int m_bufferSize;
+        private ConcurrentBag<byte[]> m_objects;
+
+        public BufferPool(int bufferSize)
+        {
+            m_bufferSize = bufferSize;
+            m_objects = new ConcurrentBag<byte[]>();
+        }
+
+        public byte[] Get()
+        {
+            byte[] buffer;
+
+            if (m_objects.TryTake(out buffer) == false)
+                buffer = new byte[m_bufferSize];
+
+            return buffer;
+        }
+
+        public void Put(byte[] buffer)
+        {
+            if (buffer == null)
+                throw new ArgumentNullException("buffer");
+
+            if (buffer.Length != m_bufferSize)
+                throw new ArgumentOutOfRangeException("buffer");
+
+            Array.Clear(buffer, 0, m_bufferSize);
+            m_objects.Add(buffer);
+        }
+    }
+}
---
+++ TcpProxy/TcpProxy/TcpProxy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpProxy
+{
+    internal sealed class TcpProxy
+    {
+        public static BufferPool BufferPool { get; private set; }
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Usage : TcpProxy <local port> <remote host> <remote port>");
+                return;
+            }
+
+            int localPort = Convert.ToInt32(args[0]);
+            string remoteHost = args[1];
+            int remotePort = Convert.ToInt32(args[2]);
+
+            BufferPool = new BufferPool(1024);
+
+            var listener = new TcpListener(IPAddress.Any, localPort);
+            listener.Start();
+
+            Console.Title = string.Concat("TcpProxy : ", remoteHost, ':', remotePort);
+            Console.WriteLine("Listening on port {0} for connections", localPort);
+
+            while (true)
+            {
+                var socket = listener.AcceptSocket();
+                new Redirector(socket, remoteHost, remotePort);
+            }
+        }
+    }
+}
